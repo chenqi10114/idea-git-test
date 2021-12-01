b0b722080337d04ba85b9b067f980370af59c382
heimaTest <heimaTest@itcast.cn>
2021-12-01 16:13:17 +0800

    Master分支，新增代码-222

---
+++ itheima-git-workspace/itheima-git-test/src/com/itheima/git/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
+
+        System.out.println("我是Master分支，新增的代码222");
+    }
+}
